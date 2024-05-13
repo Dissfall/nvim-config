27abb7d39e9909976ea60858d3377a10b149d6e5
Heorhi Lukyanau <heorhi_lukyanau@epam.com>
2024-05-13 12:04:26 +0300

    reconfigure telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,26 +1,48 @@
 return {
   {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
     config = function()
-      require("telescope").setup({
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local themes = require("telescope.themes")
+
+      telescope.setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
+            themes.get_dropdown({}),
+          },
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
+        defaults = {
+          path_display = { "smart" },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-j>"] = actions.move_selection_next,
+            },
           },
         },
       })
+
+      telescope.load_extension("ui-select")
+      telescope.load_extension("fzf")
+
       local builtin = require("telescope.builtin")
 
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = 'Find files' })
-      vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = 'Find string' })
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-
-      require("telescope").load_extension("ui-select")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files in cwd" })
+      vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in cwd" })
+      vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Find in recent files" })
     end,
   },
 }
